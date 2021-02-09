a9519becf3d61720e2956046aa7b6c339d22a0e3
JohnFreakKennedy <62018542+JohnFreakKennedy@users.noreply.github.com>
2021-02-09 15:34:37 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,50 +1,34 @@
-import math
 from random import randint
 n = int(input("Введіть розмір масиву:"))
 arr = []
 arr2 = []
-even = 0
+arr3 = []
 for i in range(n):
     arr.append(randint(0, 100))
     arr2.append(arr[i])
-    if arr[i] % 2 == 0:
-        even = even+1
+    arr3.append(arr[i])
 print(arr)
-step = 0
-temp = 0
-for j in range(n):
-    for i in range(n-1):
-        if arr[i] > arr[i+1]:
-            temp = arr[i]
-            arr[i] = arr[i+1]
-            arr[i+1] = temp
-        step = step+1
-print(arr, step, sep="\n")
-step = 0
 for i in range(n):
     for j in range(n-1):
-        if arr[j] % 2 == 1 and arr[j+1] % 2 == 0:
-            temp = arr[j]
-            arr[j] = arr[j+1]
-            arr[j+1] = temp
-        if arr[j] % 2 == 1 and arr[j + 1] % 2 == 1:
-            if arr[j] < arr[j+1]:
-                temp = arr[j]
-                arr[j] = arr[j + 1]
-                arr[j + 1] = temp
-        step = step + 1
-print(arr, step, sep="\n")
-step = 0
-step2 = 0
+        if arr[j] > arr[j+1]:
+            arr[j], arr[j+1] = arr[j+1], arr[j]
+print("Відсортований масив(бульбашка):"+str(arr))
+for i in range(n):
+    for j in range(n-1):
+        if arr2[j] % 2 == 1 and arr2[j+1] % 2 == 0:
+            arr2[j], arr2[j+1] = arr2[j+1], arr2[j]
+        if arr2[j] % 2 == 1 and arr2[j + 1] % 2 == 1:
+            if arr2[j] < arr2[j+1]:
+                arr2[j], arr2[j+1] = arr2[j+1], arr3[j]
+        if arr2[j] % 2 == 0 and arr2[j + 1] % 2 == 0:
+            if arr2[j] > arr2[j + 1]:
+                arr2[j], arr2[j + 1] = arr2[j + 1], arr2[j]
+
+print("Відсортований масив(2 завдання)"+str(arr2))
 for i in range(n):
     for j in range(n - i - 1):
-        if arr[j] > arr[j + 1]:
-            temp = arr[j]
-            arr[j] = arr[j+1]
-            arr[j+1] = temp
-        if arr2[j] > arr2[j + 1]:
-            temp = arr2[j]
-            arr2[j] = arr2[j+1]
-            arr2[j+1] = temp
-        step2 = step2 + 1
-print(arr, step, arr2, step2, sep="\n")
+        if arr3[j] > arr3[j + 1]:
+            arr3[j], arr3[j+1] = arr3[j+1], arr3[j]
+        if arr3[j] > arr3[j + 1]:
+            arr3[j], arr3[j+1] = arr3[j+1], arr3[j]
+print("Відсортований масив(покращена бульбашка): "+str(arr))
